9e55ab8eb07f097d5daa1d0537d6a13643fde171
Chmil Ruslan <chmilrv@users.noreply.github.com>
2020-09-13 14:59:13 +0300

    Добавьте файлы проекта.

---
+++ C_sharp_DZ_12/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using static System.Console;
+using System.Reflection;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+/*Разработать приложение, позволяющее определить размер диагонали монитора текущего компьютера в дюймах.*/
+namespace C_sharp_DZ_12
+{
+    public class DiagonalFromDll
+    {
+        //"C:/Windows/System32/wbem/cimwin32.dll"
+        [DllImport("cimwin32.dll")]
+        public static extern int ScreenHeight();
+
+
+    }
+
+    class Program
+    {
+
+
+
+        static void Main(string[] args)
+        {
+            Title = "C_sharp_DZ_12";
+            //Assembly a = Assembly.Load("C:/Windows/System32/wbem/cimwin32.dll");
+            try
+            {
+
+                WriteLine(DiagonalFromDll.ScreenHeight());
+
+
+            }
+            catch (Exception ex)
+            {
+                WriteLine(ex.Message);
+            }
+
+
+
+            
+
+
+
+            ReadKey();
+        }
+    }
+}
