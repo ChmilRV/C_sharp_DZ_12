438781f78da1e690bb9ad163dfafc8838052e731
Chmil Ruslan <chmilrv@users.noreply.github.com>
2020-09-14 16:52:57 +0300

    1652

--- C_sharp_DZ_12/Program.cs
+++ C_sharp_DZ_12/Program.cs
@@ -7,6 +7,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Runtime.InteropServices;
 using System.ComponentModel;
+using System.Windows.Forms;
 /*Разработать приложение, позволяющее определить размер диагонали монитора текущего компьютера в дюймах.*/
 namespace C_sharp_DZ_12
 {
@@ -15,6 +16,8 @@ namespace C_sharp_DZ_12
         [DllImport("user32.dll")]
         public static extern bool EnumDisplayMonitors(IntPtr hdc, IntPtr lprcClip, EnumMonitorsDelegate lpfnEnum, IntPtr dwData);
     }
+    
+
     [StructLayout(LayoutKind.Sequential)]
     public struct Rect
     {
@@ -35,21 +38,42 @@ namespace C_sharp_DZ_12
         public Rect WorkArea { get; set; }
     }
 
-
-
-
     
-
-    
-
+    public class DisplayInfoCollection : List<DisplayInfo>
+    {
+    }
 
 
 
     class Program
     {
 
-        
+        public DisplayInfoCollection GetDisplays()
+        {
+            DisplayInfoCollection col = new DisplayInfoCollection();
 
+            DllImportDiag.EnumDisplayMonitors(IntPtr.Zero, IntPtr.Zero,
+
+                delegate (IntPtr hMonitor, IntPtr hdcMonitor, ref Rect lprcMonitor, IntPtr dwData)
+                {
+                    MonitorInfo mi = new MonitorInfo();
+                    mi.size = (uint)Marshal.SizeOf(mi);
+                    bool success = GetMonitorInfo(hMonitor, ref mi);
+                    if (success)
+                    {
+                        DisplayInfo di = new DisplayInfo();
+                        di.ScreenWidth = (mi.monitor.right - mi.monitor.left).ToString();
+                        di.ScreenHeight = (mi.monitor.bottom - mi.monitor.top).ToString();
+                        di.MonitorArea = mi.monitor;
+                        di.WorkArea = mi.work;
+                        di.Availability = mi.flags.ToString();
+                        col.Add(di);
+                    }
+                    return true;
+                },
+                IntPtr.Zero);
+            return col;
+        }
 
 
 
@@ -64,7 +88,7 @@ namespace C_sharp_DZ_12
 
             try
             {
-               
+                
 
 
 
